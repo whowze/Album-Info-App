de0508c8be8cef1327a8c67a07ce3f26f94b8d7a
Bill Howze <williamlhowze@gmail.com>
2023-10-08 18:00:05 -0700

    Update Date and Rating
    - created a date formatter
    - created a way to show the rating

--- AlbumInfo/Models/AlbumDetailModel.swift
+++ AlbumInfo/Models/AlbumDetailModel.swift
@@ -24,12 +24,36 @@ struct Result: Codable, Hashable {
     let songTitle: String
     let albumArtwork: String
     let songUrl: String
-    let kind: String
+    let contentAdvisoryRating: String?
+    let releaseDate: String
+    
+    var formattedReleaseDate: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        if let date = dateFormatter.date(from: releaseDate) {
+            dateFormatter.dateFormat = "MM-dd-yyyy"
+            return dateFormatter.string(from: date)
+        } else {
+            return "Invalid date"
+        }
+    }
+    
+    var contentRating: String {
+        if let rating = contentAdvisoryRating {
+            return rating
+        } else {
+            return "Rating not found"
+        }
+    }
 
     enum CodingKeys: String, CodingKey {
-        case artistName, kind
+        case artistName, contentAdvisoryRating, releaseDate
         case songTitle = "name"
         case albumArtwork = "artworkUrl100"
         case songUrl = "url"
     }
+    
 }
+
+
+
